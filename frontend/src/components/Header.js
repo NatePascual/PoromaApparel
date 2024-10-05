256154d4c5dcf261c5c1e29392f8763199f830b6
NatePascual <nathan.pascual20@yahoo.com>
2024-10-06 01:30:27 +0800

    Updated the store

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,22 +5,16 @@ const Header = () => {
   return (
     <header style={headerStyle}>
       <div className="logo">Poroma Apparel</div>
-      {/* <nav>
-        <Link to="/home">Home</Link>
-        <Link to="/live-events">Live Events</Link>
-        <Link to='/exclusive-offers'>Exclusive Offers</Link>
-        <Link to='/customer-highlights'>Customer Highlights</Link>
-        <Link to="/past-sales">Past Sales</Link>
-        <Link to="/vip-signup">VIP Signup</Link>
-      </nav> */}
        <nav> 
-          <Link to="/home">Home</Link>
-          <Link to="/live-events">Live Events</Link>
-          <Link to="/exclusive-offers">Exclusive Offers</Link>
-          <Link to="/customer-highlights">Customer Highlights</Link>
-          <Link to="/past-sales">Past Sales</Link>
-          <Link to="/vip-signup">VIP Signup</Link>
-    </nav>
+        <ul className="nav-menu">
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/live-events">Live Events</Link></li>
+          <li><Link to="/exclusive-offers">Exclusive Offers</Link></li>
+          <li><Link to="/customer-highlights">Customer Highlights</Link></li>
+          <li><Link to="/past-sales">Past Sales</Link></li>
+          <li><Link to="/vip-signup">VIP Signup</Link></li>  
+        </ul>     
+      </nav>
     </header>
   );
 };
@@ -31,7 +25,6 @@ const headerStyle = {
   padding: '1rem',
   backgroundColor: '#333',
   color: '#fff',
-  width: '100%',
 };
 
 export default Header;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import eventsReducer from './eventsSlice';
+import offersReducer from './offersSlice';
+import highlightsReducer from './highlightsSlice';
+import pastSalesReducer from './pastSalesSlice';
 
 const store = configureStore({
   reducer: {
     events: eventsReducer,
+    offers: offersReducer,
+    highlights:highlightsReducer,
+    pastSales:pastSalesReducer,
   },
 });
 
